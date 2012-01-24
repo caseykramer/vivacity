c3d482746096f8aa2570fa897400dda17cdde804
Casey.Kramer <casey.kramer@gmail.com>
2012-01-23 19:56:17 -0500

    Convert tabs to spaces...thought I had set up the editor to do that...apparently that was another computer...

--- src/common/hashing/ConsistentHash.hs
+++ src/common/hashing/ConsistentHash.hs
@@ -1,7 +1,7 @@
 {-
-	A consistent hashing algorithm that is used to distribute keys amoung nodes
-	Based, mostly, on the example from 
-	http://www.fatvat.co.uk/2010/02/consistent-hashing-with-haskell.html
+    A consistent hashing algorithm that is used to distribute keys amoung nodes
+    Based, mostly, on the example from 
+    http://www.fatvat.co.uk/2010/02/consistent-hashing-with-haskell.html
 -}
 
 module ConsistentHash where
@@ -28,39 +28,39 @@ createRing nodes replicas = foldl addNode (emptyRing replicas) nodes
 
 addNode :: HashRing -> Node -> HashRing
 addNode (HashRing rep ring nodes skeys) node = HashRing rep newRing newNodes (sort newKeys) where
-	(newRing,nweKeys) = foldl (addEntry node) (ring,skeys) [0 .. rep]
-	newNodes = node: nodes
+    (newRing,nweKeys) = foldl (addEntry node) (ring,skeys) [0 .. rep]
+    newNodes = node: nodes
 
 addEntry :: Node -> (Ring,[Int]) -> Int -> (Ring,[Int])
 addEntry n (r,ks) i = (nr, key : ks) where
-	key = hashNodeId n i
-	nr  = Map.insert key n r
+    key = hashNodeId n i
+    nr  = Map.insert key n r
 
 hashNodeId :: Node -> Int -> Int
 hashNodeId node i = hashString h where
-	h = "#{" ++ node ++ "}:#{" ++ show i ++ "}"
+    h = "#{" ++ node ++ "}:#{" ++ show i ++ "}"
 
 hashString :: String -> Int
 hashString s = (fromInteger (integerDigest (sha1 (pack s))) :: Int)
 
 removeNode :: HashRing -> Node -> HashRing
 removeNode (HashRing rep ring nodes skeys) node = HashRing rep newRing newNodes newKeys where
-	(newRing,newKeys) = foldl (removeEntry node) (ring,skeys) [0 .. rep]
-	newNodes = delete node nodes
+    (newRing,newKeys) = foldl (removeEntry node) (ring,skeys) [0 .. rep]
+    newNodes = delete node nodes
 
 removeEntry :: Node -> (Ring,[Int]) -> Int -> (Ring,[Int])
 removeEntry n (r,ks) i = (nr, ks \\ [key]) where
-	key = hashNodeId n i
-	nr = Map.delete key r
+    key = hashNodeId n i
+    nr = Map.delete key r
 
 getNode :: HashRing -> String -> Maybe Node
 getNode hashRing k | isNothing x = Nothing
-				   | otherwise = Just (fst (fromJust x))
-getNode hashRing k = fst <$> getNodePos hashRing (hashString k)	
+                   | otherwise = Just (fst (fromJust x))
+getNode hashRing k = fst <$> getNodePos hashRing (hashString k) 
 
 getNodePos :: HashRing -> Int -> Maybe (Node,Int)
 getNodePos (HashRing _ ring _ skeys) ksy | Map.null ring  = Nothing
-										 | otherwise = Just (x,y)
-	where 
-		y = fromMaybe 0 (findIndex (>= key) skeys)
-		x = ring Map.! (skeys !! y)
+                                         | otherwise = Just (x,y)
+    where 
+        y = fromMaybe 0 (findIndex (>= key) skeys)
+        x = ring Map.! (skeys !! y)
--- src/common/versioning/VectorVersion.hs
+++ src/common/versioning/VectorVersion.hs
@@ -1,36 +1,36 @@
 {-
-	An implementation of Vector Versioning (or Vector Clocks) for use in 
-	versionging results of the store.
+    An implementation of Vector Versioning (or Vector Clocks) for use in 
+    versionging results of the store.
 -}
 
 module VectorVersion (
-	  Vector
-	, VectorMap
-	, getConflicts
-	, incrementVersion
-	, defaultVector) where
+      Vector
+    , VectorMap
+    , getConflicts
+    , incrementVersion
+    , defaultVector) where
 
-	import qualified Data.Map as Map
+    import qualified Data.Map as Map
 
 
-	type Vector a = (a,Int)
-	type VectorMap k = Map.Map k Int
+    type Vector a = (a,Int)
+    type VectorMap k = Map.Map k Int
 
-	hasConflicts :: VectorMap a -> VectorMap a -> Bool
-	hasConflicts v1 v2 = undefined
+    hasConflicts :: VectorMap a -> VectorMap a -> Bool
+    hasConflicts v1 v2 = undefined
 
-	getConflicts :: VectorMap a -> VectorMap a -> VectorMap a
-	getConflicts v1 v2 = undefined
+    getConflicts :: VectorMap a -> VectorMap a -> VectorMap a
+    getConflicts v1 v2 = undefined
 
-	resolveConflicts :: VectorMap a -> VectorMap a
-	resolveConflicts v = undefined
+    resolveConflicts :: VectorMap a -> VectorMap a
+    resolveConflicts v = undefined
 
-	incrementVersion :: Vector a -> Vector a
-	incrementVersion (a,b) = (a, b + 1)
+    incrementVersion :: Vector a -> Vector a
+    incrementVersion (a,b) = (a, b + 1)
 
-	toVectorMap :: Ord a => [Vector a] -> VectorMap a
-	toVectorMap vList = Map.fromList vList
+    toVectorMap :: Ord a => [Vector a] -> VectorMap a
+    toVectorMap vList = Map.fromList vList
 
-	defaultVector :: a -> Vector a
-	defaultVector a = (a,0)
+    defaultVector :: a -> Vector a
+    defaultVector a = (a,0)
 
