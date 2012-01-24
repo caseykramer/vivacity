36c698e45d402e18a9956ca371ef98e8047cc26e
Casey.Kramer <casey.kramer@gmail.com>
2012-01-24 13:24:03 -0500

    Add an implementation of 'hasConflicts' and introduce the (\?) operator to stand-in for the method.  I'm not sure the implementation is 100% correct, but initial tests with ghci seem promising

--- src/common/versioning/VectorVersion.hs
+++ src/common/versioning/VectorVersion.hs
@@ -6,31 +6,44 @@
 module VectorVersion (
       Vector
     , VectorMap
-    , getConflicts
+    , (\\?)
     , incrementVersion
     , defaultVector) where
 
-    import qualified Data.Map as Map
+import Data.Map (Map,(\\))
+import qualified Data.Map as Map
 
 
-    type Vector a = (a,Int)
-    type VectorMap k = Map.Map k Int
+type Vector a = (a,Integer)
+type VectorMap k = Map.Map k Integer
 
-    hasConflicts :: VectorMap a -> VectorMap a -> Bool
-    hasConflicts v1 v2 = undefined
+{- 
+    A conflict occurs when v2 contains either new nodes,
+    or a version that is < a node on v1.  One exception would
+    be a version of 1 on a new node (which means the first version
+    of the object from the new node).
+-}
+hasConflicts :: Ord a => VectorMap a -> VectorMap a -> Bool
+hasConflicts v1 v2 = (Map.size v1 > Map.size v2) || case (v1 \\ v2) of 
+        empty     -> vectorsAreDescendents v1 v2
+        new       -> vectorsAreNotNew new
+    
+    
+vectorsAreNotNew :: Ord a => VectorMap a -> Bool
+vectorsAreNotNew m = any (> 1) (Map.elems m)
 
-    getConflicts :: VectorMap a -> VectorMap a -> VectorMap a
-    getConflicts v1 v2 = undefined
+vectorsAreDescendents :: Ord a => VectorMap a -> VectorMap b -> Bool
+vectorsAreDescendents v1 v2 = any (\(a,b) -> b < a) (zip (Map.elems v1) (Map.elems v2))
 
-    resolveConflicts :: VectorMap a -> VectorMap a
-    resolveConflicts v = undefined
+(\\?) :: Ord a => VectorMap a -> VectorMap a -> Bool
+v1 \\? v2 = hasConflicts v1 v2
 
-    incrementVersion :: Vector a -> Vector a
-    incrementVersion (a,b) = (a, b + 1)
+incrementVersion :: Vector a -> Vector a
+incrementVersion (a,b) = (a, b + 1)
 
-    toVectorMap :: Ord a => [Vector a] -> VectorMap a
-    toVectorMap vList = Map.fromList vList
+toVectorMap :: Ord a => [Vector a] -> VectorMap a
+toVectorMap vList = Map.fromList vList
 
-    defaultVector :: a -> Vector a
-    defaultVector a = (a,0)
+defaultVector :: a -> Vector a
+defaultVector a = (a,0)
 
